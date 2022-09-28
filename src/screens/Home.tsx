7a19d0c09ee6f5fe8bb2eaba900e37f6761d001c
RafaeldeLimaThomaz <79thomaz@gmail.com>
2022-09-28 17:07:11 -0300

    TimeLine ReadME

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,32 +1,18 @@
 import React from "react";
-import { useWindowDimensions, View } from "react-native";
-import { Card } from "react-native-elements";
+import { useWindowDimensions, View, Image } from "react-native";
 
 const Home = ({ navigation }: { navigation: any }) => {
   // error
   const height = useWindowDimensions().width * 0.8;
   return (
-    <View style={{ backgroundColor: "#A7FFFB", flex: 1 }}>
-      <Card>
-        <Card.Image
-          style={{
-            height: height,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-          source={require("../../assets/Home.png")}
-        />
-      </Card>
-      <Card>
-        <Card.Image
-          style={{
-            height: height,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-          source={require("../../assets/Home.png")}
-        />
-      </Card>
+    <View style={{ backgroundColor: "#5CE1E6", flex: 1 }}>
+      <Image
+        source={require("../../assets/HomeScreen.gif")}
+        style={{
+          width: useWindowDimensions().width,
+          height: useWindowDimensions().height * 0.9,
+        }}
+      ></Image>
     </View>
   );
 };
